c5ea17d99986808eeeaa996f31d7bb54595a5433
unknown <shakhawatsalam726@gmail.com>
2024-08-24 00:34:38 +0530

    [header section is done]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body
+        className={twMerge(inter.className, "bg-black text-white antialiased")}>
+        {children}
       </body>
     </html>
   );
