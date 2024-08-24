cd78235c84ccbe3aecb8a3eaa0300914879fb5ee
unknown <shakhawatsalam726@gmail.com>
2024-08-25 02:04:03 +0530

    [logotiker animation is done]

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,10 +1,11 @@
+"use client";
 import acmeLogo from "@/assets/logo-acme.png";
-import { Purple_Purse } from "next/font/google";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import echoLogo from "@/assets/logo-echo.png";
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return (
@@ -14,9 +15,23 @@ export const LogoTicker = () => {
           <div className='flex-1 md:flex-none'>
             <h2>Trusted by top innovative terms</h2>
           </div>
-          <div className='flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
-            <div className='flex flex-none gap-14'>
+          <div className='flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+            <motion.div
+              initial={{ translateX: "-50%" }}
+              animate={{ translateX: "0" }}
+              transition={{
+                duration: 30,
+                repeat: Infinity,
+                ease: "linear",
+              }}
+              className='flex flex-none gap-14 pr-14 -translate-x-1/3'>
               {[
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                celestialLogo,
+                apexLogo,
+                quantumLogo,
                 acmeLogo,
                 pulseLogo,
                 echoLogo,
@@ -32,7 +47,7 @@ export const LogoTicker = () => {
                   className='h-6 w-auto'
                 />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
